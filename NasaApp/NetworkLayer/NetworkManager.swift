5b61f77fb54ed6df29bc39c9ee8222bf63347ef1
KrazeeKoder <tony.tulai@gmail.com>
2018-08-28 17:14:22 -0400

    fix syntax error

--- NasaApp/NetworkLayer/NetworkManager.swift
+++ NasaApp/NetworkLayer/NetworkManager.swift
@@ -24,7 +24,7 @@ class NetworkManager {
     func request(url: String, requestMethod: RequestMethod = .get, errorHandler: @escaping (_ error: Error) -> Void, successHandler: @escaping (_ json: Any) -> Void) -> URLSessionDataTask? {
         
         guard let url = URL(string: url), UIApplication.shared.canOpenURL(url) else {
-            errorHandler(NSError(domain: "Invalid URL", code: 400, userInfo: nil)
+            errorHandler(NSError(domain: "Invalid URL", code: 400, userInfo: nil))
             return nil
         }
         
